12b35c1a2506fde618816d5c25b19ef0c3c31978
Brian_Maina <bmm49@njit.edu>
2023-12-05 22:44:09 -0500

    Completed 2nd Navbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,7 @@ import { siteConfig } from "@/config/site";
 import { title, subtitle } from "@/components/primitives";
 import { GithubIcon } from "@/components/icons";
 import DefaultLayout from "@/layouts/default";
+import { Image } from "@nextui-org/react";
 import React from "react";
 import {
   Navbar,
@@ -18,6 +19,24 @@ import {
 export default function IndexPage() {
   return (
     <div>
+      <Navbar>
+        <NavbarContent className="hidden sm:flex gap-4" justify="start">
+          <NavbarBrand>
+            <p className="font-bold text-inherit">Scoops of Euphoria</p>
+            <Image width={50} alt="NextUI hero Image" src="/ice.png" />
+          </NavbarBrand>
+        </NavbarContent>
+        <NavbarContent justify="end">
+          <NavbarItem className="hidden lg:flex">
+            <Link href="#">Login</Link>
+          </NavbarItem>
+          <NavbarItem>
+            <Button as={Link} color="primary" href="#" variant="flat">
+              Sign Up
+            </Button>
+          </NavbarItem>
+        </NavbarContent>
+      </Navbar>
       <Navbar className="flex justify-center items-center">
         <NavbarItem>
           <Link color="foreground" href="#">
