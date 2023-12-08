30b02c5e4c5b9bf53d1fab7b26aad7b8b8258dc5
Brian_Maina <bmm49@njit.edu>
2023-12-08 13:30:19 -0500

    added cards to icecreampage

--- pages/icecreampage.tsx
+++ pages/icecreampage.tsx
@@ -1,14 +1,6 @@
-import { Link } from "@nextui-org/link";
-import { Snippet } from "@nextui-org/snippet";
-import { Code } from "@nextui-org/code";
-import { button as buttonStyles } from "@nextui-org/theme";
-import { siteConfig } from "@/config/site";
-import { title, subtitle } from "@/components/primitives";
-import { GithubIcon } from "@/components/icons";
-import DefaultLayout from "@/layouts/default";
-import { Image } from "@nextui-org/react";
 import React from "react";
-import styles from "../styles/hero.module.css";
+import { Card, CardBody, CardFooter, Image } from "@nextui-org/react";
+import { Link } from "@nextui-org/link";
 
 import {
   Navbar,
@@ -18,7 +10,50 @@ import {
   Button,
 } from "@nextui-org/react";
 
-const icecreampage = () => {
+export default function icecreampage() {
+  const list = [
+    {
+      title: "Orange",
+      img: "/icecream.png",
+      price: "$5.50",
+    },
+    {
+      title: "Tangerine",
+      img: "/icecream.png",
+      price: "$3.00",
+    },
+    {
+      title: "Raspberry",
+      img: "/icecream.png",
+      price: "$10.00",
+    },
+    {
+      title: "Lemon",
+      img: "/icecream.png",
+      price: "$5.30",
+    },
+    {
+      title: "Avocado",
+      img: "/icecream.png",
+      price: "$15.70",
+    },
+    {
+      title: "Lemon 2",
+      img: "/icecream.png",
+      price: "$8.00",
+    },
+    {
+      title: "Banana",
+      img: "/icecream.png",
+      price: "$7.50",
+    },
+    {
+      title: "Watermelon",
+      img: "/icecream.png",
+      price: "$12.20",
+    },
+  ];
+
   return (
     <div>
       <Navbar>
@@ -72,8 +107,31 @@ const icecreampage = () => {
           </Link>
         </NavbarItem>
       </Navbar>
+      <div className="gap-2 grid grid-cols-2 sm:grid-cols-3">
+        {list.map((item, index) => (
+          <Card
+            shadow="sm"
+            key={index}
+            isPressable
+            onPress={() => console.log("item pressed")}
+          >
+            <CardBody className="overflow-visible p-0">
+              <Image
+                shadow="sm"
+                radius="lg"
+                width="100%"
+                alt={item.title}
+                className="w-full object-cover h-[140px]"
+                src={item.img}
+              />
+            </CardBody>
+            <CardFooter className="text-small justify-between">
+              <b>{item.title}</b>
+              <p className="text-default-500">{item.price}</p>
+            </CardFooter>
+          </Card>
+        ))}
+      </div>
     </div>
   );
-};
-
-export default icecreampage;
+}
