abb6c1944a9b56e5d72b2d7c9a7e9d5a05815791
Brian_Maina <bmm49@njit.edu>
2023-12-12 17:31:09 -0500

    Added button functionality and cookie banner hides after acceptance

--- components/CookieBanner.tsx
+++ components/CookieBanner.tsx
@@ -30,7 +30,9 @@ export default function CookieBanner() {
     <div
       className={`my-10 mx-auto max-w-max md:max-w-screen-sm
                         fixed bottom-0 left-0 right-0 
-                        flex px-3 md:px-4 py-3 justify-between items-center flex-col sm:flex-row gap-4  
+                        ${
+                          cookieConsent != null ? "hidden" : "flex"
+                        }  px-3 md:px-4 py-3 justify-between items-center flex-col sm:flex-row gap-4  
                          bg-gray-700 rounded-lg shadow`}
     >
       <div className="text-center">
@@ -43,10 +45,17 @@ export default function CookieBanner() {
       </div>
 
       <div className="flex gap-2">
-        <button className="px-5 py-2 text-gray-300 rounded-md border-gray-900">
+        <button
+          className="px-5 py-2 text-gray-300 rounded-md border-gray-900"
+          onClick={() => setCookieConsent(false)}
+        >
           Decline
         </button>
-        <button className="bg-gray-900 px-5 py-2 text-white rounded-lg">
+
+        <button
+          className="bg-gray-900 px-5 py-2 text-white rounded-lg"
+          onClick={() => setCookieConsent(true)}
+        >
           Allow Cookies
         </button>
       </div>
