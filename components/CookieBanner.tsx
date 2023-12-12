1ba9d40ae33f02ca543ba654195f35cf789b36b6
Brian_Maina <bmm49@njit.edu>
2023-12-12 17:10:50 -0500

    Added react modules, specifically use state to track if users accept or deny cookies

--- components/CookieBanner.tsx
+++ components/CookieBanner.tsx
@@ -1,8 +1,31 @@
 "use client";
 
 import Link from "next/link";
+import { getLocalStorage, setLocalStorage } from "@/lib/storageHelper";
+import { useState, useEffect } from "react";
 
 export default function CookieBanner() {
+  const [cookieConsent, setCookieConsent] = useState(false);
+
+  useEffect(() => {
+    const storedCookieConsent = getLocalStorage("cookie_consent", null);
+
+    setCookieConsent(storedCookieConsent);
+  }, [setCookieConsent]);
+
+  useEffect(() => {
+    const newValue = cookieConsent ? "granted" : "denied";
+
+    window.gtag("consent", "update", {
+      analytics_storage: newValue,
+    });
+
+    setLocalStorage("cookie_consent", cookieConsent);
+
+    //For Testing
+    console.log("Cookie Consent: ", cookieConsent);
+  }, [cookieConsent]);
+
   return (
     <div
       className={`my-10 mx-auto max-w-max md:max-w-screen-sm
