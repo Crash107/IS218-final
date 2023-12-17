2885829141e36a721ee460bb854839c1d2f9c182
Brian_Maina <bmm49@njit.edu>
2023-12-16 23:41:21 -0500

    Added contact section to footer, updated one picture on icecreampage

--- layouts/default.tsx
+++ layouts/default.tsx
@@ -3,6 +3,7 @@ import { Link } from "@nextui-org/link";
 import { Head } from "./head";
 import GoogleAnalytics from "@/components/GoogleAnalytics";
 import CookieBanner from "@/components/CookieBanner";
+import { start } from "repl";
 
 export default function DefaultLayout({
   children,
@@ -18,16 +19,14 @@ export default function DefaultLayout({
         {children}
         <CookieBanner />
       </main>
-      <footer className="w-full flex items-center justify-center py-3">
-        <Link
-          isExternal
-          className="flex items-center gap-1 text-current"
-          href="https://nextui-docs-v2.vercel.app?utm_source=next-app-template"
-          title="nextui.org homepage"
-        >
-          <span className="text-default-600">Powered by</span>
-          <p className="text-primary">NextUI</p>
-        </Link>
+      <footer className="w-full flex items-center py-3">
+        <div>
+          <h1>CONTACT US</h1>
+          <h2>scoopseuphoria@gmail.com</h2>
+
+          <h2>832-734-8946</h2>
+          <h2>123 Maple Street, Newark NJ</h2>
+        </div>
       </footer>
     </div>
   );
