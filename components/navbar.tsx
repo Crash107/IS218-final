adde84706b9a072c4816505bb8ec12b6f8768a83
Brian_Maina <bmm49@njit.edu>
2023-12-16 21:31:52 -0500

    Changed text and navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -13,13 +13,17 @@ export const NavbarMain = () => {
   return (
     <div>
       <Navbar>
-        <NavbarContent justify="start">
-          <NavbarBrand>
-            <p className="font-bold text-inherit font-literata">
-              Scoops of Euphoria
-            </p>
-            <Image width={50} alt="NextUI hero Image" src="/ice.png" />
-          </NavbarBrand>
+        <NavbarBrand>
+          <p className="font-bold text-inherit">Scoops of Eurphoria</p>
+          <Image width={50} alt="NextUI hero Image" src="/ice.png" />
+        </NavbarBrand>
+
+        <NavbarContent justify="end">
+          <NavbarItem>
+            <Button as={Link} color="primary" href="/HomePage2" variant="flat">
+              Join Mailchimp
+            </Button>
+          </NavbarItem>
         </NavbarContent>
       </Navbar>
 
@@ -31,19 +35,15 @@ export const NavbarMain = () => {
         </NavbarItem>
         <NavbarItem>
           <Link href="/icecreampage" color="foreground" className="font-sans">
-            ICE CREAM CAKES
+            ICE CREAM
           </Link>
         </NavbarItem>
         <NavbarItem>
-          <Link color="foreground" href="#" className="font-sans">
+          <Link color="foreground" href="/HomePage2" className="font-sans">
             CAKES
           </Link>
         </NavbarItem>
-        <NavbarItem>
-          <Link color="foreground" href="#" className="font-sans">
-            REWARDS
-          </Link>
-        </NavbarItem>
+
         <NavbarItem>
           <Link color="foreground" href="#" className="font-sans">
             ABOUT
