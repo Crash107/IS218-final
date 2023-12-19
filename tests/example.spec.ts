361567a6ab89e6adacb72b19fc4d2ce5cc8fc191
Brian_Maina <bmm49@njit.edu>
2023-12-18 22:14:44 -0500

    Made playwright test work on local computer

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,47 +1,46 @@
 import { test, expect } from "@playwright/test";
 
-const websiteURL = "https://is-218-final-pi.vercel.app/";
-
-const websiteURL1 = "https://is-218-final-pi.vercel.app/icecreampage";
-
-const websiteURL2 = "https://is-218-final-pi.vercel.app/cakepage";
-
-const websiteURL3 = "https://is-218-final-pi.vercel.app/privacy";
+const websiteURL = "http://localhost:3000/";
+const websiteURL1 = "http://localhost:3000/icecreampage";
+const websiteURL2 = "http://localhost:3000/cakepage";
+const websiteURL3 = "http://localhost:3000/privacy";
 
 test.beforeEach(async ({ page }) => {
   await page.goto(websiteURL);
 });
 
-test("Home tab navigates to the home page", async ({ page }) => {
-  await Promise.all([
-    page.click("text=HOME"),
-    await page.waitForSelector("p.hello"),
-  ]);
-
-  expect(page.url()).toBe(websiteURL1);
-});
-
 test("Ice Cream tab navigates to the ice cream page", async ({ page }) => {
+  console.log("Before click:", page.url());
   await Promise.all([
     page.click("text=ICE CREAM"),
-    await page.waitForSelector("p.hello"),
+    page.waitForSelector("p.hello"),
+    page.waitForTimeout(5000),
   ]);
+  console.log("After click:", page.url());
 
   expect(page.url()).toBe(websiteURL1);
 });
+
 test("Cakes tab navigates to the cakes page", async ({ page }) => {
+  console.log("Before click:", page.url());
   await Promise.all([
     page.click("text=CAKES"),
-    await page.waitForSelector("p.hello"),
+    page.waitForSelector("p.hello"),
+    page.waitForTimeout(5000),
   ]);
+  console.log("After click:", page.url());
 
   expect(page.url()).toBe(websiteURL2);
 });
+
 test("Privacy tab navigates to the privacy page", async ({ page }) => {
+  console.log("Before click:", page.url());
   await Promise.all([
     page.click("text=Privacy"),
     await page.waitForSelector("p.hello"),
+    page.waitForTimeout(5000),
   ]);
+  console.log("After click:", page.url());
 
   expect(page.url()).toBe(websiteURL3);
 });
